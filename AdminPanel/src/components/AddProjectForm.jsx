bebbc0e2e9315fac5858c7480f7e3c2eff69e3ad
Onyekachukwu Muoghalu <onyekachukwumuoghalu@gmail.com>
2025-05-20 07:55:23 -0500

    Add Project form Done

--- AdminPanel/src/components/AddProjectForm.jsx
+++ AdminPanel/src/components/AddProjectForm.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import React from 'react'; 
-@import "tailwindcss";
 
 function AddProjectForm(){
     const [formdata, setformdata] = useState({
@@ -44,27 +43,92 @@ function AddProjectForm(){
         // Send techStack to backend here
     };
 
-    return(
-        <>
-            <div class="space-y-12">
-                <div class="border-b border-gray-900/10 pb-12">
-                    <h2 class="text-base/7 font-semibold text-gray-900">Add New Project</h2>
+    return(<>
+        <form onSubmit={handleSubmit}>
+            <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
+                <div className="w-full max-w-lg mx-auto border-b border-gray-900/10 pb-8">
+                    <h1 className="text-center text-base/7 font-semibold text-gray-1200">Add New Project</h1>
+                    <div className="mt-3 grid grid-cols-1 gap-x-8 gap-y-8 sm:grid-cols-6">
+                        <div className="sm:col-span-6">
 
-                    <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                        <div class="sm:col-span-4">
-                            
-                            <label for="title" class="block text-sm/6 font-medium text-gray-900">Project Title</label>
-                                <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                                    <input type="text" name="title" id="title" value={formdata.title} onChange={handleFormChange} class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="Title..."/>
+                            <label htmlFor="title" className="block text-sm/6 font-medium text-gray-900">Project Title</label>
+                                <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                                    <input type="text" name="title" id="title" value={formdata.title} onChange={handleFormChange} className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="Title..."/>
                                 </div>
                         </div>
+
+                    <div className="sm:col-span-6">
+                        <label htmlFor="desciption" className="block text-sm/6 font-medium text-gray-900">Description</label>
+                            <div className="mt-2">
+                                <textarea
+                                id="desciption"
+                                name="desciption"
+                                value={formdata.description}
+                                onChange={handleFormChange}
+                                rows={3}
+                                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"/>
+                            </div>
                     </div>
 
+                    <div className="sm:col-span-6">
 
+                        <label htmlFor="Githuburi" className="block text-sm/6 font-medium text-gray-900">Github Uri: </label>
+                            <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                                <input type="text" name="Githuburi" id="Githuburi" value={formdata.github} onChange={handleFormChange} className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="Github Url..."/>
+                            </div>
+                    </div>
+
+                    <div className="sm:col-span-6">
+
+                        <label htmlFor="StartDate" className="block text-sm/6 font-medium text-gray-900">Start Date: </label>
+                            <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                                <input type="date" name="StartDate" id="StartDate" value={formdata.starttime} onChange={handleFormChange} className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"/>
+                            </div>
+                    </div>
+
+                    <div className="sm:col-span-6">
+
+                        <label htmlFor="Update_date" className="block text-sm/6 font-medium text-gray-900">Last Update Date: </label>
+                            <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                                <input type="date" name="Update_date" id="Update_date" value={formdata.Last_updated_time} onChange={handleFormChange} className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"/>
+                            </div>
+                    </div>
+
+                    <div className="sm:col-span-6">
+                        
+                        {techStack.map((item, index) => (
+                            <div key={index} className="flex items-center gap-2">
+                                <label className="block text-sm/6 font-medium text-gray-900" htmlFor={`techstack${index + 1}`}>Technology used {index + 1}: </label>
+                                <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                                    <input
+                                        type="text"
+                                        name = {`techstack${index + 1}`}
+                                        value={item}
+                                        onChange={(e) => handleTechChanges(index, e.target.value)}
+                                        className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                                    /> 
+                                </div>
+                                {techStack.length > 1 && (
+                                    <button
+                                        type="button"
+                                        className="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-red-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 mt-2"
+                                        onClick={() => removeTechInput(index)}> Remove </button>
+                                    )}
+                            </div>))}
+
+                        <button
+                            type="button"
+                            onClick={addTechInput}
+                            className="bg-blue-500 text-white px-4 py-1 rounded hover:bg-blue-600">Add Tech</button>
+                    </div>
+                    <div className="mt-4">
+                        <button type="submit" className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"> Submit </button>
+                    </div>
                 </div>
             </div>
-        </>
-    );
+        </div>
+    </form>
+    </>);
 
 
     
